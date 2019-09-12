06583ca96b1caf4d62c56117b30c9774fb39d6f2
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2019-09-12 19:43:11 +0200

    Update Program.cs

--- GraZaDuzoZaMalo/Proceduralnie/Program.cs
+++ GraZaDuzoZaMalo/Proceduralnie/Program.cs
@@ -58,94 +58,36 @@ namespace Proceduralnie
             return propozycja;
         }
 
+        static bool Ocena(int wylosowana, int propozycja)
+        {
+            if (propozycja < wylosowana)
+            {
+                Console.WriteLine("Za mało");
+            }
+            else if (propozycja > wylosowana)
+            {
+                Console.WriteLine("Za dużo");
+            }
+            else
+            {
+                Console.WriteLine("Trafiłeś");
+                return true;
+            }
+            return false;
+        }
+
+
         static void Main(string[] args)
         {
             Start();
             int x = Losuj();
             do
             {
-                //wczytaj propozycję
+                int y = WczytajPropozycję();
                 //oceń propozycję
             }
             while (true);
         }
 
-
-
-
-        static void Main1(string[] args)
-        {
-            // 1. Komputer losuje liczbę z podanego zakresu
-            Random los = new Random();
-            int wylosowana = los.Next(1, 101);
-#if DEBUG
-            Console.WriteLine(wylosowana); // do usunięcia w Release
-#endif
-            Console.WriteLine("Wylosowałem liczbę z zakresu od 1 do 100. Odgadnij ją.");
-
-            Stopwatch czas = Stopwatch.StartNew();
-
-            // powtarzaj wielokrotnie, aż odgadnie
-            bool odgadniete = false;
-            int licznik = 0;
-            do
-            {
-                licznik++;
-                // 2. Człowiek proponuje (odgaduje)
-                Console.Write("Podaj swoją propozycję: ");
-                string napis = Console.ReadLine();
-                if (napis == "koniec")
-                {
-                    Console.WriteLine("Szkoda, że mnie opuszczasz.");
-                    return;
-                }
-
-                int propozycja = 0;
-                try
-                {
-                    propozycja = int.Parse(napis);
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Nie podano liczby.\n Spróbuj jeszcze raz");
-                    continue;
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine("Przesadziłeś. Za duża liczba.");
-                    continue;
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Niezidentyfikowany wyjątek. Awaria");
-                    Environment.Exit(1);
-                }
-
-                // 3. Komputer ocenia propozycję
-                if (propozycja < wylosowana)
-                {
-                    Console.WriteLine("Za mało");
-                }
-                else if (propozycja > wylosowana)
-                {
-                    Console.WriteLine("Za dużo");
-                }
-                else
-                {
-                    Console.WriteLine("Trafiłeś");
-                    //odgadniete = true;
-                    break;
-                }
-            }
-            //while (!odgadniete); // while( propozycja != wylosowana )
-            while (true);
-            // koniec powtarzaj
-            czas.Stop();
-
-
-            // 4. Wypisz statystyki gry
-            Console.WriteLine($"- liczba ruchów: {licznik}");
-            Console.WriteLine($"- czas gry: {czas.Elapsed}");
-        }
     }
 }
