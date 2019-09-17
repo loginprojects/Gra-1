a1f4c15438a35a638a50b3850f056af8c474c0e2
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2019-09-17 18:22:41 +0200

    Update ModelGry.cs

--- GraZaDuzoZaMalo/GraLib/ModelGry.cs
+++ GraZaDuzoZaMalo/GraLib/ModelGry.cs
@@ -8,16 +8,54 @@ namespace GraLib
 {
     public class ModelGry
     {
-        private int wylosowana;
+        readonly private int wylosowana; // readonly oznacza, że nie można zmienić wartości poza konstruktorem
         public int ZakresOd { get; private set; }
         public int ZakresDo { get; private set; }
+        public enum Stan { Trwa, Poddana, Odgadnieta }
+        public Stan StanGry { get; private set; }
 
-        public ModelGry(int zakresOd, int zakresDo)
+
+        // konstruktor
+        public ModelGry(int zakresOd = 1, int zakresDo = 100)
         {
-            ZakresOd = zakresOd;
-            ZakresDo = zakresDo;
+            ZakresOd = Math.Min( zakresOd, zakresDo );
+            ZakresDo = Math.Max( zakresOd, zakresDo );
             Random los = new Random();
             wylosowana = los.Next(ZakresOd, ZakresDo + 1);
+            StanGry = Stan.Trwa;
+        }
+
+
+        // Metody
+        public enum Odp { ZaMalo = -1, Trafione = 0, ZaDuzo = 1 };
+
+        public Odp Ocena(int propozycja)
+        {
+            if (propozycja < wylosowana)
+                return Odp.ZaMalo;
+            else if (propozycja > wylosowana)
+                return Odp.ZaDuzo;
+            else
+            {
+                StanGry = Stan.Odgadnieta;
+                return Odp.Trafione;
+            }
+        }
+
+        public void Poddaj()
+        {
+            StanGry = Stan.Poddana;
+        }
+
+        int Wylosowana
+        {
+            get
+            {
+                if (StanGry == Stan.Poddana || StanGry == Stan.Odgadnieta)
+                    return wylosowana;
+                else
+                    throw new NotSupportedException("nie wolno teraz Ci tej warotości udostępnić");
+            }
         }
 
     }
