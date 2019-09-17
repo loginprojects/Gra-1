18b066e8981509c97e15f9e30e2c36a12ddcde72
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2019-09-17 17:41:18 +0200

    ver.0

---
+++ GraZaDuzoZaMalo/GraLib/ModelGry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraLib
+{
+    public class ModelGry
+    {
+        private int wylosowana;
+        public int ZakresOd { get; private set; }
+        public int ZakresDo { get; private set; }
+
+        public ModelGry(int zakresOd, int zakresDo)
+        {
+            ZakresOd = zakresOd;
+            ZakresDo = zakresDo;
+            Random los = new Random();
+            wylosowana = los.Next(ZakresOd, ZakresDo + 1);
+        }
+
+    }
+}
